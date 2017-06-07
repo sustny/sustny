19f277e540192cbf76207ade05fc5379dd854a11
Sustny <sustny@gmail.com>
2017-06-07 12:02:06 +0900

    無駄コメント削除

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ function copyright() {
   document.addEventListener("DOMContentLoaded",function(eve){
     today = new Date();
     year = today.getFullYear();
-    /*
-    document.write('&copy; 2009 - ');
-    document.write( year );
-    document.write(' sustny');
-    */
-    //document.body.innerHTML += "&copy; 2009 - " + year + " sustny";
-    //document.body.insertAdjacentHTML("afterend", "<footer>&copy; 2009 - " + year + " sustny</footer>");
-    var footer = document.getElementById('copy');
+    footer = document.getElementById('copy');
     footer.insertAdjacentHTML("afterbegin", "&copy; 2009 - " + year + " sustny");
   },false);
 }
